13b420beed3356d32637225eb3b2f8adc208029f
LTaam <167110901+ltaamlee@users.noreply.github.com>
2024-10-11 21:17:48 +0700

    BAI1.py

--- Variables & DataType/BAI1.py
+++ Variables & DataType/BAI1.py
@@ -1,1 +1,8 @@
-
+dg=int(input("Nhap don gia: "))
+sl=int(input("Nhap soLuong: "))
+tax=0.1
+t=dg*sl
+s=t*tax
+print("{0:>26} {1:>1}*{2:>1}={3:>3}".format("Tong tien truoc thue: ",dg,sl,t))
+print("{0:>25} {1:>12}".format("Tien thue:",s))
+print("{0:>25} {1:>12}".format("Tong tien sau thue:",s+t))
