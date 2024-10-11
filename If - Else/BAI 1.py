a031db06a7fe8794660d063371d0dc4594d282b1
LTaam <167110901+ltaamlee@users.noreply.github.com>
2024-10-11 21:22:46 +0700

    BAI 1.py

---
+++ If - Else/BAI 1.py
@@ -0,0 +1,28 @@
+letter=input("Nhap diem = ")
+letter=letter.upper()
+if (letter=='A+'):
+    print(4.0)
+elif (letter=='A'):
+    print(4.0)
+elif (letter=='A-'):
+    print(3.7)
+elif (letter=='B+'):
+    print(3.3)
+elif (letter=='B'):
+    print(3.0)
+elif (letter=='B-'):
+    print(2.7)
+elif (letter=='C+'):
+    print(2.3)
+elif (letter=='C'):
+    print(2.0)
+elif (letter=='C-'):
+    print(1.7)
+elif (letter=='D+'):
+    print(1.3)
+elif (letter=='D'):
+    print(1.0)
+elif (letter=='F'):
+    print(0)
+else:
+    print("Vui long nhap diem lai!")
